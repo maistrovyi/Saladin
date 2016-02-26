151edc32a1277307250dad4afdc34dc37f3ba41d
Vladimir Maystrovoy <volmays@gmail.com>
2016-02-26 13:47:32 +0200

    jdbc12 driver + new interface

--- src/main/java/com/maystrovoy/controller/TestConnection.java
+++ src/main/java/com/maystrovoy/controller/TestConnection.java
@@ -1,39 +1,39 @@
-package com.maystrovoy.controller;
-
-import oracle.jdbc.driver.OracleDriver;
-
-import java.sql.*;
-
-public class TestConnection {
-
-    public static void main(String[] args) throws SQLException {
-
-        DriverManager.registerDriver(new OracleDriver());
-        Connection conn = DriverManager.getConnection("jdbc:Oracle:thin:@10.1.32.39:1521:MAXIMO2T","MXSAP","INTEGRATION");
-
-        Statement stmt = null;
-
-//        PreparedStatement insert = conn.prepareStatement("INSERT INTO MXSAP.QUEUE (QUEUEID,OBJECTID,OBJECTTYPE,STATUS,STARTDATE,FINISHDATE,USERNAME,MESSAGE) VALUES (9999888, 'test', 9, 'DONE', sysdate, null, 'test', null)");
-//        PreparedStatement insert = conn.prepareStatement("INSERT INTO MXSAP.QUEUE VALUES(MXSAP.QUEUE_SEQ.NEXTVAL,'test','10','ACTIVE',sysdate,NULL,'MAXIMO',NULL)");
-        PreparedStatement insert = conn.prepareStatement("INSERT INTO queue (objectid, objecttype, username, queueid, status, startdate, message) VALUES (?,10,'sapuser',queue_seq.nextval,'ACTIVE',sysdate,'')");
-        insert.setString(1, "test");
-        insert.executeUpdate();
-
-        try {
-            stmt = conn.createStatement();
-
-            ResultSet rs = stmt.executeQuery("SELECT * FROM QUEUE where queueid=9999888");
-//            ResultSet rs = stmt.executeQuery("SELECT * FROM MXSAP.QUEUE");
-
-            if (rs.next()) {
-                System.out.println("test: " + rs.getString(1));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        finally {
-            if (stmt != null) try { stmt.close(); } catch (Exception e) {}
-            if (conn != null) try { conn.close(); } catch (Exception e) {}
-        }
-    }
-}
+//package com.maystrovoy.controller;
+//
+//import oracle.jdbc.driver.OracleDriver;
+//
+//import java.sql.*;
+//
+//public class TestConnection {
+//
+//    public static void main(String[] args) throws SQLException {
+//
+//        DriverManager.registerDriver(new OracleDriver());
+//        Connection conn = DriverManager.getConnection("jdbc:Oracle:thin:@10.1.32.39:1521:MAXIMO2T","MXSAP","INTEGRATION");
+//
+//        Statement stmt = null;
+//
+////        PreparedStatement insert = conn.prepareStatement("INSERT INTO MXSAP.QUEUE (QUEUEID,OBJECTID,OBJECTTYPE,STATUS,STARTDATE,FINISHDATE,USERNAME,MESSAGE) VALUES (9999888, 'test', 9, 'DONE', sysdate, null, 'test', null)");
+////        PreparedStatement insert = conn.prepareStatement("INSERT INTO MXSAP.QUEUE VALUES(MXSAP.QUEUE_SEQ.NEXTVAL,'test','10','ACTIVE',sysdate,NULL,'MAXIMO',NULL)");
+//        PreparedStatement insert = conn.prepareStatement("INSERT INTO queue (objectid, objecttype, username, queueid, status, startdate, message) VALUES (?,10,'sapuser',queue_seq.nextval,'ACTIVE',sysdate,'')");
+//        insert.setString(1, "test");
+//        insert.executeUpdate();
+//
+//        try {
+//            stmt = conn.createStatement();
+//
+//            ResultSet rs = stmt.executeQuery("SELECT * FROM QUEUE where queueid=9999888");
+////            ResultSet rs = stmt.executeQuery("SELECT * FROM MXSAP.QUEUE");
+//
+//            if (rs.next()) {
+//                System.out.println("test: " + rs.getString(1));
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        finally {
+//            if (stmt != null) try { stmt.close(); } catch (Exception e) {}
+//            if (conn != null) try { conn.close(); } catch (Exception e) {}
+//        }
+//    }
+//}
--- src/main/webapp/WEB-INF/views/header.jsp
+++ src/main/webapp/WEB-INF/views/header.jsp
@@ -1,21 +1,20 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
- <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>lifecell</title>
-  <link rel="stylesheet" type="text/css" href="resources/css/style.css">
- </head>
- <body>
-  <div id="head">
-   <h1>LIFECELL</h1>
-  </div>
-  <div id="menu">
-   <div><a href="/">HOME PAGE</a></div>
-   <div class="line"></div>
-   <div><a href="/check_stock" name="check_stock">CHECK STOCK</a></div>
-   <div><a href="/action_2" name="action_2">ACTION 2</a></div>
-   <div><a href="/action_3" name="action_3">ACTION 3</a></div>
-  </div>
- </body>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>lifecell</title>
+    <link rel="stylesheet" type="text/css" href="resources/css/style.css">
+</head>
+<body>
+<div id="head">
+</div>
+<div id="menu">
+    <div><a href="/">HOME PAGE</a></div>
+    <div class="line"></div>
+    <div><a href="/check_stock" name="check_stock">CHECK STOCK</a></div>
+    <div><a href="/action_2" name="action_2">ACTION 2</a></div>
+    <div><a href="/action_3" name="action_3">ACTION 3</a></div>
+</div>
+</body>
 </html>
